ab8dabc28a1f7f1d26e05bf8bc23a331d1ffa4a1
Mr_Jerry <jerrykano@gmail.com>
2021-12-19 19:45:16 -0800

    Before Helper Func

--- db/seed.js
+++ db/seed.js
@@ -5,7 +5,6 @@ const { client } = require('./index');
 
 async function testDB() {
   try {
-    // connect the client to the database, finally
     client.connect();
 
     const { rows } = await client.query(`SELECT * FROM users;`);
@@ -13,51 +12,9 @@ async function testDB() {
   } catch (error) {
     console.error(error);
   } finally {
-    // it's important to close out the client connection
     client.end();
   }
 }
 
-testDB();
 
-// this function should call a query which drops all tables from our database
-    async function dropTables() {
-        try {
-          await client.query(`
-            DROP TABLE IF EXISTS users;
-          `);
-        } catch (error) {
-          throw error;
-        }
-      }
-    
-  
-  // this function should call a query which creates all tables for our database 
-  async function createTables() {
-    try {
-      await client.query(`
-      CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
-        username varchar(255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL
-      );
-      `);
-    } catch (error) {
-      throw error; // we pass the error up to the function that calls createTables
-    }
-  }
-  
-  async function rebuildDB() {
-    try {
-      client.connect();
-  
-      await dropTables();
-      await createTables();
-    } catch (error) {
-      console.error(error);
-    } finally {
-      client.end();
-    }
-  }
-  
-  rebuildDB();
+testDB();
