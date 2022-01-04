5c2c004684803613876a237c94c20be4e9044ad3
Mr_Jerry <jerrykano@gmail.com>
2022-01-03 18:13:46 -0800

    Part 2:1 Complete

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-// inside db/index.js
+const PORT = 3000;
+const express = require('express');
+const server = express();
 
-async function getAllUsers() {
-    const { rows } = await client.query(
-      `SELECT id, username 
-      FROM users;
-    `);
-  
-    return rows;
-  }
-  
-  // and export them
-  module.exports = {
-    client,
-    getAllUsers,
-  }
+const { client } = require('./db');
+client.connect();
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+server.use(express.json())
+
+server.use((req, res, next) => {
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
+
+  next();
+});
